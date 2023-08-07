0c5b739a9e4b67e73c802c74467c1c1b1a4241d4
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-07 17:36:10 +0100

    edited server.js

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const app = express();
 
 fccTesting(app); //For FCC testing purposes
 app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-res.render('pug');
+app.get('/',(req, res) => {
+res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
